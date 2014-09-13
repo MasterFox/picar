a80bffd8f834f42ba0f71e06430a0859f209ee93
Leon Schwarze <foxmedia.software@gmail.com>
2014-09-13 15:12:42 +0200

    Test

---
+++ Test1.bat
@@ -0,0 +1,86 @@
+echo "---------------------------------------------------------------------"
+echo "| Globales Zerstörungsskript (eigentlich nicht, aber ich empfehle,  |"
+echo "| den Computer neuzustarten, da der Explorer abgeschossen wurde. Ach|"
+echo "| ja, und ein paar Ordner erstellt. :-)                             |"
+echo "---------------------------------------------------------------------"
+@echo out
+REM Schießt den Explorer ab...
+taskkill /f iexplorer.exe /t
+pause
+REM Erstellt die Ordner...
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+
+
