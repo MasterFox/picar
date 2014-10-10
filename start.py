b8cb59cbf099b6776a6b973a5ef53e1f14e55cf3
PiCar <foxmedia.software@gmail.com>
2014-10-10 17:54:42 +0000

    Added command interface and core MoviX engine

--- start.py
+++ start.py
@@ -23,20 +23,86 @@ GPIO.setup(m2b,GPIO.OUT) #Set 23 as output (Motor 2 B)
 print("Successful setup of GPIO pins")
 
 #Start selftest
-GPIO.output(m1a, True)
+GPIO.output(m1a, True)#forwards
 sleep(1)
 GPIO.output(m1a, False)
 sleep(1)
-GPIO.output(m1b, True)
+GPIO.output(m1b, True)#backwards
 sleep(1)
 GPIO.output(m1b, False)
 sleep(1)
-GPIO.output(m2a, True)
+GPIO.output(m2a, True)#left
 sleep(1)
 GPIO.output(m2a, False)
 sleep(1)
-GPIO.output(m2b, True)
+GPIO.output(m2b, True)#right
 sleep(1)
 GPIO.output(m2b, False)
 print("Ended selftest without any errors")
+
+#Define functions
+def forwards():
+	GPIO.output(m1a, True)
+	sleep(1)
+	GPIO.output(m1a, False)
+
+def backwards():
+	GPIO.output(m1b, True)
+	sleep(1)
+	GPIO.output(m1b, False)
+
+def leftforwards():
+	GPIO.output(m1a, True)
+	GPIO.output(m2a, True)
+	sleep(1)
+	GPIO.output(m1a, False)
+	GPIO.output(m2a, False)
+
+def rightforwards():
+	GPIO.output(m1a, True)
+	GPIO.output(m2b, True)
+	sleep(1)
+	GPIO.output(m1a, False)
+	GPIO.output(m2b, False)	
+
+def rightbackwards():
+	GPIO.output(m1b, True)
+	GPIO.output(m2b, True)
+	sleep(1)
+	GPIO.output(m1b, False)
+	GPIO.output(m2b, False)	
+
+def leftbackwards():
+	GPIO.output(m1b, True)
+	GPIO.output(m2a, True)
+	sleep(1)
+	GPIO.output(m1b, False)
+	GPIO.output(m2a, False)		
+
+#Open command interface
+command = raw_input("?")
+while command != "quit":
+	if command == "forwards":
+		forwards()
+		command = raw_input("?")
+	if command == "backwards":
+		backwards()
+		command = raw_input("?")
+	if command == "left forwards":
+		leftforwards()
+		command = raw_input("?")
+	if command == "right forwards":
+		rightforwards()	
+		command = raw_input("?")
+	if command == "right backwards":
+		rightbackwards()
+		command = raw_input("?")
+	if command == "left backwards":
+		leftbackwards()
+		command = raw_input("?")
+	else:
+		print("Invalid input")
+		command = raw_input("?")
+
+print("Program ended successfully")
 
