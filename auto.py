b8326057f9b76fbd92085473dbcf0e965c2ba3f2
Leon Schwarze <foxmedia.software@gmail.com>
2014-12-06 12:31:49 +0100

    Bug fix

--- auto.py
+++ auto.py
@@ -30,7 +30,7 @@ motor2_b = 23
 lighting = 24
 usonic_trig = 25
 usonic_echo = 27
-distance = 0
+distance = global navix_distance
 
 #Setup the outputs
 GPIO.setup(motor1_a,GPIO.OUT) #Set 17 as output (Motor 1 A)
